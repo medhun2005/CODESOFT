ddeff4a591602ff39f67e90605d5652dcd8e87ff
medhun2005 <144113485+medhun2005@users.noreply.github.com>
2024-02-24 00:48:38 +0530

    Add files via upload

---
+++ atm_interface/src/atm_interface/Atm_interface.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atm_interface;
+
+/**
+ *
+ * @author Dell
+ */
+public class Atm_interface {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+    
+}
+}
